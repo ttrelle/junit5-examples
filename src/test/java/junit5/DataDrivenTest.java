aa3575d5b35ac33ef9db554a656252e96fbe4309
Tobias Trelle <tobias.trelle@codecentric.de>
2017-09-20 15:15:30 +0200

    added data-driven test

---
+++ src/test/java/junit5/DataDrivenTest.java
@@ -0,0 +1,16 @@
+package junit5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class DataDrivenTest {
+
+	@ParameterizedTest(name = "{index}: {0} + {1} = {2}")
+	@CsvFileSource(resources = "/addition.csv")
+	void addition(int a, int b, int c) {
+		assertTrue(a + b == c);
+	}
+	
+}
