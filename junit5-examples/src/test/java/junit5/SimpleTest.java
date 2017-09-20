6aacd49f06432b26d659a375476c3373457c21a2
tobias <tobias@localhost>
2017-09-20 14:21:15 +0200

    Initial version

---
+++ junit5-examples/src/test/java/junit5/SimpleTest.java
@@ -0,0 +1,28 @@
+package junit5;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+public class SimpleTest {
+
+	@BeforeEach void setUp(TestInfo ti) {
+		out(ti.getDisplayName());
+	}
+	
+	@Test 
+	@DisplayName(";-)")
+	@Tag("wichtig") 	@Tag("mathematisch")
+	void foo(TestInfo ti) {
+		assertTrue(.9 == (float).8 + .1, () -> "Addition kaputt" );
+		out("Tags: " + ti.getTags());
+	}
+	
+	static void out(Object o) {
+		System.out.println(o);
+	}
+}
